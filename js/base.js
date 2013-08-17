c3c6169b4260f09f6424349823d6632ba1eac385
Jonas Heinrich <onny@project-insanity.org>
2013-08-17 20:32:12 +0000

    initial commit

---
+++ js/base.js
@@ -0,0 +1,370 @@
+var iso639 = {
+    'aa': 'Afar',
+    'ab': 'Abkhaz',
+    'ae': 'Avestan',
+    'af': 'Afrikaans',
+    'ak': 'Akan',
+    'am': 'Amharic',
+    'an': 'Aragonese',
+    'ar': 'Arabic',
+    'as': 'Assamese',
+    'av': 'Avaric',
+    'ay': 'Aymara',
+    'az': 'Azerbaijani',
+    'ba': 'Bashkir',
+    'be': 'Belarusian',
+    'bg': 'Bulgarian',
+    'bh': 'Bihari',
+    'bi': 'Bislama',
+    'bm': 'Bambara',
+    'bn': 'Bengali',
+    'bo': 'Tibetan Standard',
+    'br': 'Breton',
+    'bs': 'Bosnian',
+    'ca': 'Catalan',
+    'ce': 'Chechen',
+    'ch': 'Chamorro',
+    'co': 'Corsican',
+    'cr': 'Cree',
+    'cs': 'Czech',
+    'cu': 'Old Slavonic',
+    'cv': 'Chuvash',
+    'cy': 'Welsh',
+    'da': 'Danish',
+    'de': 'German',
+    'dv': 'Divehi; Dhivehi; Maldivian;',
+    'dz': 'Dzongkha',
+    'ee': 'Ewe',
+    'el': 'Greek, Modern',
+    'en': 'English',
+    'eo': 'Esperanto',
+    'es': 'Spanish; Castilian',
+    'et': 'Estonian',
+    'eu': 'Basque',
+    'fa': 'Persian',
+    'ff': 'Fula; Fulah; Pulaar; Pular',
+    'fi': 'Finnish',
+    'fj': 'Fijian',
+    'fo': 'Faroese',
+    'fr': 'French',
+    'fy': 'Western Frisian',
+    'ga': 'Irish',
+    'gd': 'Scottish Gaelic; Gaelic',
+    'gl': 'Galician',
+    'gn': 'GuaranÃ­',
+    'gu': 'Gujarati',
+    'gv': 'Manx',
+    'ha': 'Hausa',
+    'he': 'Hebrew (modern)',
+    'hi': 'Hindi',
+    'ho': 'Hiri Motu',
+    'hr': 'Croatian',
+    'ht': 'Haitian; Haitian Creole',
+    'hu': 'Hungarian',
+    'hy': 'Armenian',
+    'hz': 'Herero',
+    'ia': 'Interlingua',
+    'id': 'Indonesian',
+    'ie': 'Interlingue',
+    'ig': 'Igbo',
+    'ii': 'Nuosu',
+    'ik': 'Inupiaq',
+    'io': 'Ido',
+    'is': 'Icelandic',
+    'it': 'Italian',
+    'iu': 'Inuktitut',
+    'ja': 'Japanese (ja)',
+    'jv': 'Javanese (jv)',
+    'ka': 'Georgian',
+    'kg': 'Kongo',
+    'ki': 'Kikuyu, Gikuyu',
+    'kj': 'Kwanyama, Kuanyama',
+    'kk': 'Kazakh',
+    'kl': 'Kalaallisut, Greenlandic',
+    'km': 'Khmer',
+    'kn': 'Kannada',
+    'ko': 'Korean',
+    'kr': 'Kanuri',
+    'ks': 'Kashmiri',
+    'ku': 'Kurdish',
+    'kv': 'Komi',
+    'kw': 'Cornish',
+    'ky': 'Kirghiz, Kyrgyz',
+    'la': 'Latin',
+    'lb': 'Luxembourgish, Letzeburgesch',
+    'lg': 'Luganda',
+    'li': 'Limburgish, Limburgan, Limburger',
+    'ln': 'Lingala',
+    'lo': 'Lao',
+    'lt': 'Lithuanian',
+    'lu': 'Luba-Katanga',
+    'lv': 'Latvian',
+    'mg': 'Malagasy',
+    'mh': 'Marshallese',
+    'mi': 'Maori',
+    'mk': 'Macedonian',
+    'ml': 'Malayalam',
+    'mn': 'Mongolian',
+    'mr': 'Marathi (Mara?hi)',
+    'ms': 'Malay',
+    'mt': 'Maltese',
+    'my': 'Burmese',
+    'na': 'Nauru',
+    'nb': 'Norwegian BokmÃ¥l',
+    'nd': 'North Ndebele',
+    'ne': 'Nepali',
+    'ng': 'Ndonga',
+    'nl': 'Dutch',
+    'nn': 'Norwegian Nynorsk',
+    'no': 'Norwegian',
+    'nr': 'South Ndebele',
+    'nv': 'Navajo, Navaho',
+    'ny': 'Chichewa; Chewa; Nyanja',
+    'oc': 'Occitan',
+    'oj': 'Ojibwe, Ojibwa',
+    'om': 'Oromo',
+    'or': 'Oriya',
+    'os': 'Ossetian, Ossetic',
+    'pa': 'Panjabi, Punjabi',
+    'pi': 'Pali',
+    'pl': 'Polish',
+    'ps': 'Pashto, Pushto',
+    'pt': 'Portuguese',
+    'qu': 'Quechua',
+    'rm': 'Romansh',
+    'rn': 'Kirundi',
+    'ro': 'Romanian, Moldavian, Moldovan',
+    'ru': 'Russian',
+    'rw': 'Kinyarwanda',
+    'sa': 'Sanskrit (Sa?sk?ta)',
+    'sc': 'Sardinian',
+    'sd': 'Sindhi',
+    'se': 'Northern Sami',
+    'sg': 'Sango',
+    'si': 'Sinhala, Sinhalese',
+    'sk': 'Slovak',
+    'sl': 'Slovene',
+    'sm': 'Samoan',
+    'sn': 'Shona',
+    'so': 'Somali',
+    'sq': 'Albanian',
+    'sr': 'Serbian',
+    'ss': 'Swati',
+    'st': 'Southern Sotho',
+    'su': 'Sundanese',
+    'sv': 'Swedish',
+    'sw': 'Swahili',
+    'ta': 'Tamil',
+    'te': 'Telugu',
+    'tg': 'Tajik',
+    'th': 'Thai',
+    'ti': 'Tigrinya',
+    'tk': 'Turkmen',
+    'tl': 'Tagalog',
+    'tn': 'Tswana',
+    'to': 'Tonga (Tonga Islands)',
+    'tr': 'Turkish',
+    'ts': 'Tsonga',
+    'tt': 'Tatar',
+    'tw': 'Twi',
+    'ty': 'Tahitian',
+    'ug': 'Uighur, Uyghur',
+    'uk': 'Ukrainian',
+    'ur': 'Urdu',
+    'uz': 'Uzbek',
+    've': 'Venda',
+    'vi': 'Vietnamese',
+    'vo': 'VolapÃ¼k',
+    'wa': 'Walloon',
+    'wo': 'Wolof',
+    'xh': 'Xhosa',
+    'yi': 'Yiddish',
+    'yo': 'Yoruba',
+    'za': 'Zhuang, Chuang',
+    'zh': 'Chinese',
+    'zu': 'Zulu'
+}
+
+function get_attributes(trans) {
+    var wortart = '[e]';
+    $.ajax({
+	type: 'GET',
+        url: 'http://' + $('#to').val() + '.wiktionary.org/w/api.php',
+	async: false,
+	cache: false,
+        timeout: 30000,
+        data: {
+            action: 'parse',
+            prop: 'templates',
+            format: 'json',
+            page: trans
+        },
+        dataType: 'jsonp',
+        success: function(data) {
+		<!--alert('test');-->
+		wortart = '[m]';
+	},
+	error: function(data) {
+		alert('test');
+	}
+    });
+	return wortart;
+}
+
+function show_result(data) {
+    $("#result tbody > tr").remove();
+    $.each(data, function(parse, content) {
+      if (parse == 'error') { // response is an error, e.g. page does not exist
+	  $('#result tbody').append('<tr><td colspan="2" align="center">Sorry, the page <b><a target=new href="http://'+$('#from').val()+'.wiktionary.org/wiki/'+ $('#word').val().replace(' ','_') +'">' + $('#word').val() +'</b></a> does not exists in the Wiktionary. If you want to create it, start here!</td></tr>');
+	  return false; // break
+      }
+      if (parse == 'parse') { // valid page, continue
+	$.each(content.iwlinks, function(i, el) {
+	    if (el['prefix'] == $('#to').val()) { // there's an entry with our requested langcode in the translation table
+	      var trans = el['*'];
+	      trans = trans.replace($('#to').val()+":", '').replace('_', ' ').replace('Special:Search/','');
+	      $('#result tbody').append('<tr><td><a target=new href="http://'+$('#from').val()+'.wiktionary.org/wiki/'+ $('#word').val().replace(' ','_') +'">' + $('#word').val() +'</a></td><td><a target=new href="http://'+$('#to').val()+'.wiktionary.org/wiki/'+ trans.replace(' ','_') +'">' + trans + '</a> <font color=blue>[-]</font></td></tr>');
+	      $('#result tbody').append('<tr class="alt"><td><a target=new href="http://'+$('#from').val()+'.wiktionary.org/wiki/'+ $('#word').val().replace(' ','_') +'">' + $('#word').val() +'</a></td><td><a target=new href="http://'+$('#to').val()+'.wiktionary.org/wiki/'+ trans.replace(' ','_') +'">' + trans + '</a> <font color=blue>[-]</font></td></tr>');
+	    }
+	}); 
+	if ($('#result tbody tr').length == 0) { // function each quits without returning true: page exists, but not including our requested language
+	  $('#result tbody').append('<tr><td colspan="2" align="center">Sorry, the page <b><a target=new href="http://'+$('#from').val()+'.wiktionary.org/wiki/'+ $('#word').val().replace(' ','_') +'">' + $('#word').val() +'</b></a> exists, but no translation in your chosen language was given. If you want to add a translation, simply click <a target=new href=#>here</a>!</td></tr>');
+	}
+      }
+    });
+}
+
+function setSearchHistory(from, to, word) {
+	var stateObj = {from: from, to: to, word: word };
+	window.document.title = "WikiDict.cc - Search: " + word;	
+	history.pushState(stateObj, "WikiDict.cc - Search: " + word, "?from="+ from + "&to=" + to + "&q=" + word); 
+}
+
+function translate(from, to, word) {
+	$.ajax({
+		url: 'http://' + from + '.wiktionary.org/w/api.php',
+		data: {
+			action: 'parse',
+			prop: 'templates|iwlinks',
+			format: 'json',
+			//iwlimit: 30,
+			//iwprefix: to,
+			page: word
+		},
+		dataType: 'jsonp',
+	        success: show_result
+    	});
+    	return false;
+}
+
+function article(page) {
+	setArticleHistory(page);
+	showArticle(page);
+}
+
+function showArticle(page) {
+	$('#result').hide();
+	$('section > article').show();
+	$('section > article').load(page);
+}
+
+function setArticleHistory(page) {
+	window.document.title = "WikiDict.cc - " + page;	
+	history.pushState({page: page}, "WikiDict.cc - " + page, page + ".html");
+}
+
+function setOpensearch(from, to) {
+	$("#opensearch").attr("title", "WikiDict.cc " + from + "-" + to);
+	$("#opensearch").attr("href", "opensearch.php?from=" + from + "&amp;to=" + to);
+}
+function addOpensearch(){
+	if (window.external && ('AddSearchProvider' in window.external)) {
+		window.external.AddSearchProvider("http://wikidict.cc/opensearch.php?from=" + $('#from').val() + "&to=" + $('#to').val());
+	} else {
+		alert ("Your browser does not support the AddSearchProvider method!");
+	}
+}
+
+$(window).load(function(){
+
+      $(".dropdown img.flag").addClass("flagvisibility");
+
+            $(".dropdown dt a").click(function() {
+                $(".dropdown dd ul").toggle();
+            });
+                        
+            $(".dropdown dd ul li a").click(function() {
+                var text = $(this).html();
+                $(".dropdown dt a span").html(text);
+                $(".dropdown dd ul").hide();
+            });
+                        
+            function getSelectedValue(id) {
+                return $("#" + id).find("dt a span.value").html();
+            }
+
+            $(document).bind('click', function(e) {
+                var $clicked = $(e.target);
+                if (! $clicked.parents().hasClass("dropdown"))
+                    $(".dropdown dd ul").hide();
+            });
+
+
+            $("#flagSwitcher").click(function() {
+                $(".dropdown img.flag").toggleClass("flagvisibility");
+            });
+
+
+  	$('form').submit(function() {
+		var from = $('#from').val();
+		var to = $('#to').val();
+		var word = $('#word').val();
+
+		setSearchHistory(from, to, word);
+		translate(from, to, word);
+		setOpensearch(from, to);
+		$( "#word" ).autocomplete( "close" ); //auto-completion fenster schließen
+		return false;
+	});
+  	$('#word').focus();
+	$("#word").autocomplete({
+	    source: function(request, response) {
+		$.ajax({
+		    url: "http://"+$('#from').val()+".wiktionary.org/w/api.php",
+		    dataType: "jsonp",
+		    data: {
+			'action': "opensearch",
+			'format': "json",
+			'search': request.term
+		    },
+		    success: function(data) {
+			response(data[1]);
+		    }
+		});
+	    }
+	});
+	$( "#word" ).on( "autocompleteselect", function( event, ui ) {
+		$('#word').val(ui.item['value']); //insert the selected value into the field because of dependencies
+		$('form').submit();
+	});
+});
+
+window.onpopstate = function(event) {
+	if(event.state != undefined) {
+		if(event.state['page'] != undefined) {
+			showArticle(event.state['page']);
+		} else {
+			var from = event.state['from'];
+			var to = event.state['to'];
+			var word = event.state['word'];
+			$('#word').val(word);
+			window.document.getElementById('from').value = from;
+			window.document.getElementById('to').value = to;
+			setOpensearch(from, to);
+			translate(from, to, word);
+		}
+	} else {
+			$('section > article').hide();
+			$('#result').show();
+	}
+};
